4cb8444c6dd62eaf3740ac48a8d91840b8ccff5f
Gabriele Musco <gabmus@disroot.org>
2021-11-06 00:41:32 +0100

    put the loading bar inside an overlay to avoid shifting the content

--- gfeeds/revealer_loading_bar.py
+++ gfeeds/revealer_loading_bar.py
@@ -2,10 +2,11 @@ from gi.repository import Gtk
 
 
 class RevealerLoadingBar(Gtk.Revealer):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self):
+        super().__init__(transition_type=Gtk.RevealerTransitionType.CROSSFADE)
 
         self.progress_bar = Gtk.ProgressBar()
+        self.progress_bar.get_style_context().add_class('osd')
         self.set_child(self.progress_bar)
         self.set_fraction = self.progress_bar.set_fraction
         self.set_reveal_child(True)
