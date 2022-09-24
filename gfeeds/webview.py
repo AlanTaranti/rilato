77082ee5c1cb47e78dab4f327bd97eb8feedea9b
Gabriele Musco <gabmus@disroot.org>
2022-09-24 13:07:56 +0200

    fix high cpu usage during overscroll

--- gfeeds/webview.py
+++ gfeeds/webview.py
@@ -171,8 +171,11 @@ class GFeedsWebView(Gtk.Stack):
         self.webkitview_settings.set_enable_javascript(
             self.confman.conf['enable_js']
         )
-        self.webkitview_settings.set_enable_smooth_scrolling(False)
+        self.webkitview_settings.set_enable_smooth_scrolling(True)
         self.webkitview_settings.set_enable_page_cache(True)
+        self.webkitview_settings.set_enable_back_forward_navigation_gestures(
+            True
+        )
         self.webkitview_settings.set_enable_accelerated_2d_canvas(True)
         self.webkitview.set_settings(self.webkitview_settings)
 
