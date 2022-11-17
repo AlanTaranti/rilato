a5161c9a84ca28defe0f9cf2fb2aeb53eeac4c03
Gabriele Musco <gabmus@disroot.org>
2022-11-17 07:34:42 +0100

    update run script to take gsettings into account

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export GSETTINGS_SCHEMA_DIR="$PWD/build/build/testdir/share/glib-2.0/schemas"
 ninja -C build
 ninja -C build install
-ninja -C build run
+
+./build/build/testdir/bin/gfeeds
