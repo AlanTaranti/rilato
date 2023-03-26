2e897978137b7bd278149beda572986acc92129c
Gabriele Musco <gabmus@disroot.org>
2023-03-26 14:20:48 +0200

    require webkit 6 in tests init

--- test/__init__.py
+++ test/__init__.py
@@ -3,4 +3,4 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Gdk', '4.0')
 gi.require_version('Adw', '1')
-gi.require_version('WebKit2', '5.0')
+gi.require_version('WebKit', '6.0')
