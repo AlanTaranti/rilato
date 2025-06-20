20147f8cdd19f2309f99d43bb964b46ec804cd31
Alan Taranti <contato@alantaranti.me>
2025-06-19 23:13:38 -0400

    chore: add text input and send button to main window UI, implement basic input handling

--- src/window.py
+++ src/window.py
@@ -25,6 +25,24 @@ class RilatoWindow(Adw.ApplicationWindow):
     __gtype_name__ = 'RilatoWindow'
 
     label = Gtk.Template.Child()
+    startup_entry = Gtk.Template.Child()
+    send_button = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        # Clear entry at startup
+        self.startup_entry.set_text("")
+
+    @Gtk.Template.Callback
+    def on_send_button_clicked(self, widget):
+        self._send_input()
+
+    @Gtk.Template.Callback
+    def on_startup_entry_activate(self, entry):
+        self._send_input()
+
+    def _send_input(self):
+        text = self.startup_entry.get_text()
+        # TODO: handle sending text
+        print(f"Sending input: {text}")
+        self.startup_entry.set_text("")
